3b21ef3156e0d43a3893a3b5259ef897cbbe1ba0
Eterna <one.eternity@gmail.com>
2017-12-28 22:08:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 // write your code below!
 
-var name;
-var holiday;
-var days;
+
 
 function happyHolidays() {
-  return 'Happy holidays!';
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!';
+  return "Happy holidays, "+name+"!";
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!';
+  return "Happy ${holiday}, ${name}!";
 }
 
 function holidayCountdown(holiday, days) {
